a6e3183f41c24f76ac59a93e4c8794e28e7e18fc
Kola Kolade <kolakolade@gmail.com>
2022-12-07 08:26:07 +0100

    Elements selected from dom and put in variables

---
+++ main.js
@@ -0,0 +1,13 @@
+// VARIABLES
+
+// Designate a variable to store the books
+let awesomeBookDB;
+
+// Select element from the dom
+const library = document.querySelector('.library')
+const bookShelf = document.querySelector('.book-shelf');
+const title = document.querySelector('input#book-title');
+const author = document.querySelector('input#book-author');
+const addBookForm = document.querySelector('.add-book-form')
+const addBookBtn = document.querySelector('.add-book');
+
