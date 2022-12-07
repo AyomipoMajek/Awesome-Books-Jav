325fda720ed9b61bbb1fa964fa19d269af4c7063
Kola Kolade <kolakolade@gmail.com>
2022-12-07 09:18:05 +0100

    Add function and event listener to remove book from the dom

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 let awesomeBookDB;
 
 // Select element from the dom
-const library = document.querySelector('.library')
+const library = document.querySelector('.library');
 const bookShelf = document.querySelector('.book-shelf');
 const title = document.querySelector('input#book-title');
 const author = document.querySelector('input#book-author');
-const addBookForm = document.querySelector('.add-book-form')
+const addBookForm = document.querySelector('.add-book-form');
 const addBookBtn = document.querySelector('.add-book');
 
 // FUNCTIONS
@@ -16,39 +16,46 @@ const addBookBtn = document.querySelector('.add-book');
 const addBook = (e) => {
   e.preventDefault()
 // Define individual book
-const book = {
-  title: title.value,
-  author: author.value
-}
+  const book = {
+    title: title.value,
+    author: author.value
+  };
 // Create elements required for individual book
-const bookCard = document.createElement('div');
-const bookTitle = document.createElement('p');
-const bookAuthor =  document.createElement('p');
-const delBtn = document.createElement('button');
+  const bookCard = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor =  document.createElement('p');
+  const delBtn = document.createElement('button');
 
 // Assigning class, attributes and text content to the elements
-bookCard.className = 'book';
- bookShelf.className = 'book-shelf'
- bookTitle.className = 'book-title';
- bookTitle.textContent = book.title;
- bookAuthor.className = 'book-author';
- bookAuthor.textContent = book.author;
- 
- delBtn.className = 'delete-button'
- delBtn.setAttribute('type', 'button')
- delBtn.textContent = 'Delete Book'
- 
- const hr = document.createElement('hr');
- 
- bookCard.append(bookTitle, bookAuthor, delBtn, hr);
- bookShelf.appendChild(bookCard)
+  bookCard.className = 'book';
+  bookShelf.className = 'book-shelf';
+  bookTitle.className = 'book-title';
+  bookTitle.textContent = book.title;
+  bookAuthor.className = 'book-author';
+  bookAuthor.textContent = book.author;
+  
+  delBtn.className = 'delete-button'
+  delBtn.setAttribute('type', 'button')
+  delBtn.textContent = 'Delete Book';
+  
+  const hr = document.createElement('hr');
+  
+  bookCard.append(bookTitle, bookAuthor, delBtn, hr);
+  bookShelf.appendChild(bookCard)
 
 
- title.value = '';
- author.value = '';
-console.log(bookShelf);
+  title.value = '';
+  author.value = '';
+  console.log(bookShelf);
 }
 
+// Function to remove book from DOM
+const removeBook = (e) => {
+  // e.stopPropagation();
+  if (e.target.classList.contains('delete-button')) {
+    e.target.parentElement.remove();
+  }
+}
 
 
 // EVENTLISTENERS
@@ -57,7 +64,12 @@ console.log(bookShelf);
 
 // Function to load all event listeners
 const loadEventListeners = () => {
-addBookForm.addEventListener('submit', addBook)
+// Event listener to add book on form submit
+addBookForm.addEventListener('submit', addBook);
+
+// Event listener to remove book on delete button clicked
+bookShelf.addEventListener('click', removeBook);
+
 }
 
 loadEventListeners();
