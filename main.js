4be8b7e2e9b394db513c927c1f7eb929bbf7559a
Kola Kolade <kolakolade@gmail.com>
2022-12-07 13:18:33 +0100

    Add funtion to remove book from DOM, work in progress on function to remove book from localStorage

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// VARIABLES
+// VARIABLEs
+// ------------------------
 
 // Designate a variable to store the books
 let awesomeBookDB;
@@ -12,7 +13,9 @@ const addBookForm = document.querySelector('.add-book-form');
 const addBookBtn = document.querySelector('.add-book');
 
 // FUNCTIONS
-// function to get localStorage content
+// ---------------------------
+
+//1. function to get localStorage content
 const getLocalStorageContent = () => {
   let LSContent;
   if (localStorage.getItem('books') === null) {
@@ -24,46 +27,30 @@ const getLocalStorageContent = () => {
 }
 
 
-// Function to add data to localStorage
+//2. Function to add data to localStorage
 const addBookToLS = (element) => {
   awesomeBookDB = getLocalStorageContent()
   awesomeBookDB.push(element);
   localStorage.setItem('books', JSON.stringify(awesomeBookDB))
   }
-// const addBookToLS = (element) => {
-//   if (localStorage.getItem('books') === null){
-//     awesomeBookDB = [];  
-//   } else {
-//     awesomeBookDB = JSON.parse(localStorage.getItem('books')); 
-//   }
-//   awesomeBookDB.push(element);
-//   localStorage.setItem('books', JSON.stringify(awesomeBookDB))
-//   }
 
 
-
-
-
-
-// function to remove book from localStorage 
-const removeBookFromLS = () => {
-
-}
-  
-
-// Function to add book to DOM
+//3. Function to add book to DOM
 const addBook = (e) => {
   e.preventDefault()
 // Define individual book
   const book = {
     title: title.value,
-    author: author.value
+    author: author.value,
+    id: Math.random()
   };
 // Create elements required for individual book
   const bookCard = document.createElement('div');
   const bookTitle = document.createElement('p');
   const bookAuthor =  document.createElement('p');
+  const bookId = document.createElement('span')
   const delBtn = document.createElement('button');
+  const hr = document.createElement('hr');
 
 // Assigning class, attributes and text content to the elements
   bookCard.className = 'book';
@@ -72,38 +59,83 @@ const addBook = (e) => {
   bookTitle.textContent = book.title;
   bookAuthor.className = 'book-author';
   bookAuthor.textContent = book.author;
-  
-  delBtn.className = 'delete-button'
-  delBtn.setAttribute('type', 'button')
+  bookId.innerText = book.id;
+  bookId.setAttribute('style', 'display: none;');
+  delBtn.className = 'delete-button';
+  delBtn.setAttribute('type', 'button');
   delBtn.textContent = 'Delete Book';
   
-  const hr = document.createElement('hr');
-  
-  bookCard.append(bookTitle, bookAuthor, delBtn, hr);
-  bookShelf.appendChild(bookCard)
+  // Build a book card and attach it to the library
+  bookCard.append(bookTitle, bookAuthor, bookId, delBtn, hr);
+  bookShelf.appendChild(bookCard);
 
-  addBookToLS(book)
+  // Add the book to localStorage
+  addBookToLS(book);
 
   title.value = '';
   author.value = '';
-  console.log(bookShelf);
 }
 
 
-// Function to remove book from DOM
+//4. Function to remove book from DOM
 const removeBook = (e) => {
+  // Remove book from DOM
   if (e.target.classList.contains('delete-button')) {
     e.target.parentElement.remove();
   }
+  // Remove book from local storage
+  let idItemToRemove = Number(e.target.previousSibling.innerText);
+  console.log(idItemToRemove);
+  const indexITR = getLocalStorageContent().forEach((el, i, Arr) => {
+    if( el.id === idItemToRemove) {
+      
+      console.log(i);
+    }
+  }) 
+
 }
 
 
+//5. Function to load localStorage content and render it on Document load
+const loadLStoDOM = () => {
 
+  const book = {
+    title: title.value,
+    author: author.value,
+    id: Math.random()
+  };
 
+  getLocalStorageContent().forEach(el => {
+    // Create elements required for individual book
+  const bookCard = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor =  document.createElement('p');
+  const bookId = document.createElement('span')
+  const delBtn = document.createElement('button');
+  const hr = document.createElement('hr');
+
+// Assigning class, attributes and text content to the elements
+  bookCard.className = 'book';
+  bookShelf.className = 'book-shelf';
+  bookTitle.className = 'book-title';
+  bookTitle.textContent = el.title;
+  bookAuthor.className = 'book-author';
+  bookAuthor.textContent = el.author;
+  bookId.innerText = el.id;
+  bookId.setAttribute('style', 'display: none;') 
+  delBtn.className = 'delete-button';
+  delBtn.setAttribute('type', 'button');
+  delBtn.textContent = 'Delete Book';
+  
+  
+  bookCard.append(bookTitle, bookAuthor, bookId, delBtn, hr);
+  bookShelf.appendChild(bookCard)
+
+  });
+}
 
 // EVENT LISTENERS
-
-
+// ------------------------
 
 // Function to load all event listeners
 const loadEventListeners = () => {
@@ -113,6 +145,8 @@ addBookForm.addEventListener('submit', addBook);
 // Event listener to remove book on delete button clicked
 bookShelf.addEventListener('click', removeBook);
 
+// Event listener to load localStorage content on DOM load
+document.addEventListener('DOMContentLoaded', loadLStoDOM)
 }
 
 loadEventListeners();
